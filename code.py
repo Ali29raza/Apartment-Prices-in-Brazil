7e94cf826fc06db3940c9db55a932a73a396dc9c
Ali Raza <ali29raza@gmail.com>
2024-03-15 01:01:49 +0500

    changed to heading

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-"""In this project, I am going to work with a dataset of homes for sale in Brazil.
-The goal is to determine if there are regional differences in the real estate market.
-Also, I will look at southern Brazil to see if there is a relationship between home size and price."""
+###In this project, I am going to work with a dataset of homes for sale in Brazil.
+###The goal is to determine if there are regional differences in the real estate market.
+###Also, I will look at southern Brazil to see if there is a relationship between home size and price.
 
